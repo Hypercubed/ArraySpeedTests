6e83ff937970a14a67fc90c78a664f18e74cf13f
J. Harshbarger <hypercubed@gmail.com>
2014-12-03 23:41:03 +0900

    Using fork for each bench

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-require('./bench/filter.js').run();
-require('./bench/forEach.js').run();
-require('./bench/reduce.js').run();
-require('./bench/map.js').run();
-require('./bench/indexOf.js').run();
+var cp = require('child_process');
+
+var files = [
+  './bench/reduce.js',
+  './bench/map.js',
+  './bench/indexOf.js',
+  './bench/forEach.js',
+  './bench/filter.js',
+  './bench/concat.js'
+];
+
+function runNext(code) {
+  if (code == 0 && files.length > 0) {
+    cp
+      .fork(files.shift())
+      .on('close', runNext);
+  }
+}
+
+runNext(0);
